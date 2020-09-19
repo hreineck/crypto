2aad0258d795af573baad0678b7fca254e81b866
hreineck <43356777+hreineck@users.noreply.github.com>
2020-09-18 22:23:59 -0400

    Update shift.py

--- shift.py
+++ shift.py
@@ -1,4 +1,5 @@
-ciphertext = list("lcllewljazlnnzmvyiylhrmhza")
+#brute force a shift cipher
+ciphertext = list("ciphertext")
 key = 7
 for i in range(len(ciphertext)):
     ciphertext[i] = chr((ord(ciphertext[i]) - 97 - key)%26 + 97)
