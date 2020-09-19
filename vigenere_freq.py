d8654de7676f74a603471b861bd2971c02951190
hreineck <43356777+hreineck@users.noreply.github.com>
2020-09-18 22:28:58 -0400

    Update vigenere_freq.py

--- vigenere_freq.py
+++ vigenere_freq.py
@@ -1,5 +1,6 @@
+#gets the frequency of every letter for a given vigenere key length
 length = 5
-ciphertext = list("xkjurowmllpxwznpimbvbqjcnowxpcchhvvfvsllfvxhazityxohulxqojaxelxzxmyjaqfstsrulhhucdskbxknjqidallpqslluhiaqfpbpcidsvcihwhwewthbtxrljnrsncihuvffuxvoukjljswmaqfvjwjsdyljogjxdboxajultucpzmpliwmlubzxvoodybafdskxgqfadshxnxehsaruojaqfpfkndhsaafvulluwtaqfrupwjrszxgpfutjqiynrxnyntwmhcukjfbirzsmehhsjshyonddzzntzmplilrwnmwmlvuryonthuhabwnvw")
+ciphertext = list("fdjasjfdsaf")
 for i in range(length):
     count = [0]*26
     print("column", i + 1)
